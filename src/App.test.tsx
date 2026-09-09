87751c6513b8e5eebda846c3e77268a1d4d15f0a
PasinduUmayanga <pasinduumayanga93@gmail.com>
2026-09-09 19:44:52 +0530

    Update App.test.tsx to test for the login with Azure button instead of the learn react link

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login with azure button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginButton = screen.getByRole('button', { name: /loginwithazure/i });
+  expect(loginButton).toBeInTheDocument();
 });
